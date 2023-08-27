f26675e72beeddf596230ee37a887144d13500ee
mossorri <emmanuelkusiboamah@gmail.com>
2023-08-26 22:35:15 -0700

    implemented search function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 import "./App.css";
+// import SearchMovie from "./SearchMovie";
+import searchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
 const API_URL = "https://www.omdbapi.com?apikey=ea715774";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data)
+    setMovies(data.Search);
   };
-  useEffect(() => {
-    searchMovies('spider-man')
-  }, []);
 
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
 
-  return <div className="App">
-    <h1>App</h1>
-  </div>;
+      {/* <SearchMovie /> */}
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={searchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie}/>
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/SearchMovie.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import searchIcon from "./search.svg";
+
+
+// function SearchMovie() {
+//   return (
+//       <div className="search">
+//         <input
+//           placeholder="Search for movies"
+//           value={searchTerm}
+//           onChange={(e) => setsearchTerm(e.target.value)}
+//         />
+//         <img src={searchIcon} alt="search" onClick={() =>searchMovies(searchTerm)} />
+//       </div>
+//   );
+// }
+
+// export default SearchMovie;
